aad40f68308572a3bb2ab9a9f5060ddeea86242e
Maksim Litvinov <metalim@gmail.com>
2024-12-16 15:45:57 +0200

    build all and time all

--- run_all.sh
+++ run_all.sh
@@ -1,9 +1,18 @@
 #!/usr/bin/env bash
+
+echo "Building all..."
 for folder in [0-2][0-9]/; do
   if [ -d "$folder" ]; then
     echo $folder
-    cd $folder
-    go run . ${1:-input.txt}
-    cd ..
+    go build -o bin/ ./$folder
+  fi
+done
+
+echo "Running all..."
+time for bin in bin/*; do
+  if [ -f "$bin" ]; then
+    echo
+    echo $(basename $bin)
+    $bin "$(basename $bin)/${1:-input.txt}"
   fi
 done
