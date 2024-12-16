7f26bc943f26a535f8fe6eb7b01f70839bb3d5c3
Maksim Litvinov <metalim@gmail.com>
2024-12-16 05:08:30 +0200

    alternative map implementation for day 6

--- 06/main.go
+++ 06/main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"bytes"
 	"flag"
 	"fmt"
+	"maps"
 	"os"
 	"runtime"
 	"strings"
@@ -20,11 +22,27 @@ func catch(err error) {
 }
 
 type Guard struct {
-	y, x int
-	dir  int
+	p   Vec2
+	dir int
 }
 type Vec2 struct {
 	y, x int
+}
+
+func (v Vec2) Add(o Vec2) Vec2 {
+	return Vec2{v.y + o.y, v.x + o.x}
+}
+
+type Grid struct {
+	Map map[Vec2]rune
+	BR  Vec2
+}
+
+func (g *Grid) Clone() *Grid {
+	return &Grid{
+		Map: maps.Clone(g.Map),
+		BR:  g.BR,
+	}
 }
 
 var Directions = []Vec2{
@@ -50,25 +68,31 @@ func main() {
 	part2(grid, guard)
 }
 
-func parseInput(input string) (grid [][]rune, guard Guard) {
+func parseInput(input string) (grid *Grid, guard Guard) {
 	lines := strings.Split(input, "\n")
 	if len(lines[len(lines)-1]) == 0 {
 		lines = lines[:len(lines)-1]
 	}
 
+	grid = &Grid{
+		Map: make(map[Vec2]rune),
+		BR:  Vec2{len(lines), len(lines[0])},
+	}
 	var guardFound bool
 	for y, line := range lines {
-		grid = append(grid, []rune(line))
-		x := strings.Index(line, "^")
-		if x >= 0 {
-			if guardFound {
-				panic("Multiple guards found")
+		for x, cell := range line {
+			if cell == '#' {
+				grid.Map[Vec2{y, x}] = cell
 			}
-			guardFound = true
-			guard = Guard{
-				y:   y,
-				x:   x,
-				dir: 0,
+			if cell == '^' {
+				if guardFound {
+					panic("Multiple guards found")
+				}
+				guardFound = true
+				guard = Guard{
+					p:   Vec2{y, x},
+					dir: 0,
+				}
 			}
 		}
 	}
@@ -78,52 +102,52 @@ func parseInput(input string) (grid [][]rune, guard Guard) {
 	return grid, guard
 }
 
-func walkOut(grid [][]rune, guard Guard) map[Vec2]struct{} {
-	var H = len(grid)
-	var W = len(grid[0])
+func walkOut(grid *Grid, guard Guard) map[Vec2]struct{} {
 	path := map[Vec2]struct{}{}
 	for {
-		path[Vec2{guard.y, guard.x}] = struct{}{}
-		grid[guard.y][guard.x] = 'X'
-		y := guard.y + Directions[guard.dir].y
-		x := guard.x + Directions[guard.dir].x
-		if y < 0 || H <= y || x < 0 || W <= x {
+		path[guard.p] = struct{}{}
+		grid.Map[guard.p] = 'X'
+		np := guard.p.Add(Directions[guard.dir])
+		if np.y < 0 || grid.BR.y <= np.y || np.x < 0 || grid.BR.x <= np.x {
 			return path
 		}
-		if grid[y][x] == '#' {
+		if grid.Map[np] == '#' {
 			guard.dir = (guard.dir + 3) % 4 // Turn right
 		} else {
-			guard.y = y
-			guard.x = x
+			guard.p = np
 		}
 	}
 }
 
-func printGrid(grid [][]rune) {
-	if Print {
-		for _, line := range grid {
-			fmt.Println(string(line))
-		}
+func printGrid(grid *Grid) {
+	if !Print {
+		return
 	}
-}
-func part1(grid [][]rune, guard Guard) {
-	timeStart := time.Now()
-	walkOut(grid, guard)
-	var count int
-	for _, line := range grid {
-		for _, cell := range line {
-			if cell == 'X' {
-				count++
+	fmt.Println(grid.BR)
+	var buf bytes.Buffer
+	for y := 0; y < grid.BR.y; y++ {
+		for x := 0; x < grid.BR.x; x++ {
+			r, ok := grid.Map[Vec2{y, x}]
+			if !ok {
+				r = ' '
 			}
+			buf.WriteRune(r)
 		}
+		buf.WriteRune('\n')
 	}
+	os.Stdout.Write(buf.Bytes())
+}
+
+func part1(grid *Grid, guard Guard) {
+	timeStart := time.Now()
+	path := walkOut(grid, guard)
 	printGrid(grid)
-	fmt.Printf("Part 1: \t\t%d\tin %v\n", count, time.Since(timeStart))
+	fmt.Printf("Part 1: \t\t%d\tin %v\n", len(path), time.Since(timeStart))
 }
 
 var Workers = runtime.NumCPU()
 
-func part2(grid [][]rune, guard Guard) {
+func part2(grid *Grid, guard Guard) {
 	timeStart := time.Now()
 	path := walkOut(grid, guard)
 	fmt.Printf("Using %d workers\n", Workers)
@@ -132,24 +156,18 @@ func part2(grid [][]rune, guard Guard) {
 	wg.Add(Workers)
 	ch := make(chan Vec2, Workers)
 	for i := 0; i < Workers; i++ {
-		dupe := make([][]rune, len(grid))
-		for y, line := range grid {
-			dupe[y] = make([]rune, len(line))
-			copy(dupe[y], line)
-		}
+		dupe := grid.Clone()
 
-		go func(grid [][]rune) {
+		go func(grid *Grid) {
 			defer wg.Done()
 			visited := map[Guard]struct{}{}
 			for p := range ch {
-				y, x := p.y, p.x
-				grid[y][x] = '#'
-				clear(visited)
+				grid.Map[p] = '#'
 				if hasLoop(grid, guard, visited) {
 					loopCount.Add(1)
-					grid[y][x] = 'O'
+					grid.Map[p] = 'O'
 				} else {
-					grid[y][x] = ' '
+					grid.Map[p] = ' '
 				}
 			}
 		}(dupe)
@@ -163,25 +181,21 @@ func part2(grid [][]rune, guard Guard) {
 	fmt.Printf("Part 2: \t\t%d\tin %v\n", loopCount.Load(), time.Since(timeStart))
 }
 
-func hasLoop(grid [][]rune, guard Guard, visited map[Guard]struct{}) bool {
+func hasLoop(grid *Grid, guard Guard, visited map[Guard]struct{}) bool {
 	clear(visited)
-	var H = len(grid)
-	var W = len(grid[0])
 	for {
 		if _, ok := visited[guard]; ok {
 			return true
 		}
 		visited[guard] = struct{}{}
-		y := guard.y + Directions[guard.dir].y
-		x := guard.x + Directions[guard.dir].x
-		if y < 0 || H <= y || x < 0 || W <= x {
+		np := guard.p.Add(Directions[guard.dir])
+		if np.y < 0 || grid.BR.y <= np.y || np.x < 0 || grid.BR.x <= np.x {
 			return false
 		}
-		if grid[y][x] == '#' {
+		if grid.Map[np] == '#' {
 			guard.dir = (guard.dir + 3) % 4 // Turn right
 		} else {
-			guard.y = y
-			guard.x = x
+			guard.p = np
 		}
 	}
 }
