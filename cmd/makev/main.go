5b052a664762477a77e900ef57b16ed2a6479d34
Maksim Litvinov <metalim@gmail.com>
2024-12-22 01:18:17 +0200

    update makev and prompt

--- cmd/makev/main.go
+++ cmd/makev/main.go
@@ -1,26 +1,39 @@
 package main
 
 import (
-	"flag"
 	"fmt"
 	"os"
 	"path/filepath"
+	"strconv"
 )
 
 func main() {
-	flag.Parse()
-	if flag.NArg() == 0 {
-		fmt.Println("Usage: makev <name>")
-		os.Exit(1)
+	version := 1
+	name := "v1"
+	for {
+		if _, err := os.Stat(filepath.Join("o1", name)); os.IsNotExist(err) {
+			break
+		}
+		version++
+		name = "v" + strconv.Itoa(version)
 	}
-
-	name := "v" + flag.Arg(0)
 	folder := filepath.Join("o1", name)
 	err := os.MkdirAll(folder, 0755)
 	catch(err)
 	err = os.WriteFile(filepath.Join(folder, name+".go"), nil, 0644)
 	catch(err)
-	err = os.WriteFile(filepath.Join(folder, name+".txt"), nil, 0644)
+	if version > 1 {
+		err = os.WriteFile(filepath.Join(folder, name+".txt"), nil, 0644)
+		catch(err)
+		return
+	}
+	prompt, err := os.ReadFile("../o1/prompt.txt")
+	catch(err)
+	prompt = append(prompt, "\n"...)
+	task, err := os.ReadFile("task.txt")
+	catch(err)
+	prompt = append(prompt, task...)
+	err = os.WriteFile(filepath.Join(folder, name+".txt"), prompt, 0644)
 	catch(err)
 }
 
